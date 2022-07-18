dd89856e41d17cb00c5c03eaa1e2f1f66dd9051c
Natalya Belikova <minaevanv@inbox.ru>
2022-07-19 01:10:45 +0300

    Получение-отправка данных через сервер

---
+++ src/components/profile.js
@@ -0,0 +1,46 @@
+import { getProfileInfo, updateProfileInfo, updateAvatar } from './api.js';
+import { openAvatarPopup } from './modal.js';
+
+const name = document.querySelector('.profile__name');
+const description = document.querySelector('.profile__description');
+const avatar = document.querySelector('.avatar');
+const avatarImg = avatar.querySelector('.avatar__img');
+let idProfile;
+
+avatar.addEventListener('click', openAvatarPopup);
+
+function saveProfileInfo(newName, newDescription) {
+  updateProfileInfo(newName, newDescription)
+    .then(res => res.json())
+    .then(res => {
+      setProfileData(res.name, res.about);
+    })
+}
+
+function setNewAvatar(img) {
+  updateAvatar(img)
+  .then(res => res.json())
+  .then(res => {
+    setAvatarImg(res.avatar);
+  });
+}
+
+getProfileInfo()
+  .then(res => res.json())
+  .then((res) => {
+    setProfileData(res.name, res.about);
+    setAvatarImg(res.avatar);
+    idProfile = res._id;
+  });
+
+
+function setAvatarImg(url) {
+  avatarImg.src = url;
+}
+
+function setProfileData(nameProfile, aboutProfile) {
+  name.textContent = nameProfile;
+  description.textContent = aboutProfile;
+}
+
+export { saveProfileInfo, idProfile, setNewAvatar };
